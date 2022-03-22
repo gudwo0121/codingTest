716ccf6f633174298defa9e437e1000367cfb54f
BRO <91590390+gudwo0121@users.noreply.github.com>
2022-03-22 15:33:48 +0900

    22.03.22.Link

---
+++ CodingTest/CodingTest/src/greedy/GymSuit.java
@@ -0,0 +1,25 @@
+package greedy;
+
+public class GymSuit {
+
+	// case 1
+	static int n = 5;
+	static int[] lost = { 2, 4 };
+	static int[] reserve = { 1, 3, 5 };
+
+	public static void main(String[] args) {
+		GymSuit gymSuit = new GymSuit();
+		int answer = gymSuit.solution1(n, lost, reserve);
+		System.out.println(answer);
+	}
+
+	// solution 1
+	public int solution1(int n, int[] lost, int[] reserve) {
+		int answer = 0;
+		
+		
+
+		return answer;
+	}
+
+}
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ****
 
 * [완주하지 못한 선수](https://github.com/gudwo0121/codingTest/blob/master/CodingTest/CodingTest/src/hash/NotArrivedRunner.java)
+* [체육복]()
 
 ***
 
