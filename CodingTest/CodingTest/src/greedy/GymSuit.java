96f156002e9c40798735a9d99412360879af825b
BRO <91590390+gudwo0121@users.noreply.github.com>
2022-04-05 00:44:43 +0900

    22.04.05.JoyStickSolved.needMoreInfo

--- CodingTest/CodingTest/src/greedy/GymSuit.java
+++ CodingTest/CodingTest/src/greedy/GymSuit.java
@@ -32,7 +32,7 @@ public class GymSuit {
 	// 배열을 만들어 학생 체육복 현황을 0 기준으로 없음(-1), 있음(0), 많음(+1)로 표현
 	// 배열에서 (앞뒤 번호 학생 존재 && 그 학생이 여벌이 있음) = 나(+1), 걔(-1) 끝까지 1회 반복
 	public int solution1(int n, int[] lost, int[] reserve) {
-		
+
 		// 총 학생 수로 시작
 		int answer = n;
 		int[] student = new int[n];
@@ -49,16 +49,16 @@ public class GymSuit {
 
 			// 한벌도 없는 학생 중에
 			if (student[i] == -1) {
-				
+
 				// 바로 앞 번호 학생이 존재하고
-				// 그 학생이 두벌 있는 학생이면 
+				// 그 학생이 두벌 있는 학생이면
 				if (i - 1 >= 0 && student[i - 1] == 1) {
 					// 나는 한벌 빌리고
 					student[i]++;
 					// 그 학생은 한벌 빌려준다
 					student[i - 1]--;
 				}
-				
+
 				// 바로 뒷 번호 학생이 존재하고
 				// 그 학생이 두벌 있는 학생이면
 				else if (i + 1 < student.length && student[i + 1] == 1) {
@@ -67,10 +67,10 @@ public class GymSuit {
 					// 그 학생은 한벌 빌려준다
 					student[i + 1]--;
 				}
-				
+
 				// 앞뒤 번호 학생이 없거나
 				// 있다고 해도 그 학생이 두벌 있는 학생이 아니라면
-				// 별짓을 한다해도 못빌림 = 총 학생 수에서 감소(-1) 
+				// 별짓을 한다해도 못빌림 = 총 학생 수에서 감소(-1)
 				else
 					answer--;
 			}
--- CodingTest/CodingTest/src/greedy/JoyStick.java
+++ CodingTest/CodingTest/src/greedy/JoyStick.java
@@ -1,19 +1,15 @@
 package greedy;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class JoyStick {
 
 	// case 1
-//	static String name = "JEROEN";
+	static String name = "JEROEN";
 
 	// case 2
 //	static String name = "JAN";
 
 	// case 3
-	static String name = "JAZN";
+//	static String name = "JAZN";
 
 	public static void main(String[] args) {
 		JoyStick joyStick = new JoyStick();
@@ -22,56 +18,30 @@ public class JoyStick {
 	}
 
 	// solution 1
-	// 뒤로가냐 앞으로가냐 = 첫번째 조건
-	// 낭비되는 동선인가 판단 = 두번째 조건 = 사이에 A 개수?
-	// (왔던길 + 끝부터 A아닌것까지 길이) & ()
-	// 전자가 크면 앞으로 ㄱ, 후자가 크면 뒤로 ㄱ (이건 경우에 따라 다를듯?)
 	public int solution1(String name) {
-		int ud = 0;
-		int lr = 0;
-		int cursor = 0;
-		int r = 0;
-		int l = 0;
-		int[] arr = new int[name.length()];
-		ArrayList<String> list = new ArrayList<>();
+		int answer = 0;
+		int length = name.length();
 
-		for (String spelling : name.split(""))
-			list.add(spelling);
+		int index;
+		int move = length - 1;
 
-		System.out.println(list);
+		for (int i = 0; i < name.length(); i++) {
+			// 상하 이동 횟수
+			answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
 
-		int i = 0;
-		for (String spelling : list) {
-			if (spelling.equals("A")) {
-				arr[i++] = 0;
-			} else {
-				arr[i++] = (int) spelling.charAt(0) - 65;
+			// 현재 위치 바로 앞에
+			index = i + 1;
+			// 커서가 이동할 공간이 있고, 그 알파벳이 'A'이 아닐때까지 반복하며
+			while (index < length && name.charAt(index) == 'A') {
+				// 연속되는 'A'의 개수를 누적
+				index++;
 			}
+
+			// move : 앞으로 쭉가는 경우
+			// 현위치 * 2 + ('A'가 아닌 알파벳 개수) : 뒤로 돌아가는 경우
+			move = Math.min(move, i * 2 + length - index);
+			move = Math.min(move, (length - index) * 2 + i);
 		}
-
-		System.out.println(Arrays.toString(arr));
-		System.out.println(IntStream.of(cursor).sum());
-
-		while (IntStream.of(cursor).sum() != 0) {
-			l = 0;
-			r = 0;
-
-			// 우측방향 예상 이동거리
-			for (int j = cursor; j < name.length(); j++) {
-				if (arr[j] != 0) {
-					r = j - cursor;
-					System.out.println("우측 : " + r);
-					break;
-				}
-			}
-
-			// 좌측방향 예상 이동거리 = 지금까지 이동거리 + 끝부터 첫 문자 만나기까지 거리
-			for (int j = name.length(); j >= 0; j--) {
-				
-			}
-
-		}
-
-		return (lr + ud);
+		return answer + move;
 	}
 }
