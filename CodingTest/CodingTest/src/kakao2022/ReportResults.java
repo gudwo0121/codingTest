ed3302535d204334e74ec6ac7d1789595d645884
BRO <91590390+gudwo0121@users.noreply.github.com>
2022-04-14 22:44:11 +0900

    22.04.14.ReportHalf

--- CodingTest/CodingTest/src/kakao2022/ReportResults.java
+++ CodingTest/CodingTest/src/kakao2022/ReportResults.java
@@ -2,12 +2,14 @@ package kakao2022;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class ReportResults {
 
 	// case 1
 	static String[] id_list = { "muzi", "frodo", "apeach", "neo" };
-	static String[] report = { "muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi", "neo muzi" };
+	static String[] report = { "muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi" };
 	static int k = 2;
 
 	// case 2
@@ -17,11 +19,11 @@ public class ReportResults {
 
 	public static void main(String[] args) {
 		ReportResults reportResults = new ReportResults();
-		int[] answer = reportResults.solution1(id_list, report, k);
+		int[] answer = reportResults.solution2(id_list, report, k);
 		System.out.println(Arrays.toString(answer));
 	}
 
-	// solution 1 not solved
+	// solution 1
 	// 해시셋으로 중복을 제거하라 + 이중 반복문말고 하나씩 나눠서 정리하라
 	public int[] solution1(String[] id_list, String[] report, int k) {
 		int[] answer = new int[id_list.length];
@@ -61,10 +63,50 @@ public class ReportResults {
 				answer[i++] = mailMap.get(id);
 			}
 		}
-		
+
 		System.out.println(reportMap);
 		System.out.println(mailMap);
 
+		return answer;
+	}
+
+	// solution 2
+	public int[] solution2(String[] id_list, String[] report, int k) {
+		int[] answer = new int[id_list.length];
+		HashSet<String> set = new HashSet<String>(Arrays.asList(report));
+		Iterator itr = set.iterator();
+		HashMap<String, String> map = new HashMap<String, String>();
+
+		while (itr.hasNext()) {
+			String temp = (String) itr.next();
+			String[] s = temp.split(" ");
+			if (map.containsKey(s[1])) {
+				String d = map.get(s[1]);
+				map.put(s[1], d + " " + s[0]);
+			} else {
+				map.put(s[1], s[0]);
+			}
+		}
+
+		Iterator entries = map.entrySet().iterator();
+		while (entries.hasNext()) {
+			HashMap.Entry entry = (HashMap.Entry) entries.next();
+			String key = (String) entry.getKey();
+			String value = (String) entry.getValue();
+			System.out.println(key + " " + value);
+			String[] white = value.split(" ");
+
+			if (white.length >= k) {
+
+				for (int i = 0; i < white.length; i++) {
+					for (int j = 0; j < id_list.length; j++) {
+						if (white[i].equals(id_list[j])) {
+							answer[j] += 1;
+						}
+					}
+				}
+			}
+		}
 		return answer;
 	}
 }
