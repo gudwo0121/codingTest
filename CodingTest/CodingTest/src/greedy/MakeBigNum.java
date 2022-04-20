48a5d4f18f1de28aa51348a1704180ec82653a65
BRO <91590390+gudwo0121@users.noreply.github.com>
2022-04-20 20:59:08 +0900

    22.04.20.makeBigNum.Linked

---
+++ CodingTest/CodingTest/src/greedy/MakeBigNum.java
@@ -0,0 +1,29 @@
+package greedy;
+
+public class MakeBigNum {
+
+	// case 1
+	static String number = "1924";
+	static int k = 2;
+
+	// case 2
+//	static String number = "1231234";
+//	static int k = 3;
+
+	// case 3
+//	static String number = "4177252841";
+//	static int k = 4;
+
+	public static void main(String[] args) {
+		MakeBigNum makeBigNum = new MakeBigNum();
+		String answer = makeBigNum.solution1(number, k);
+		System.out.println(answer);
+	}
+
+	// solution 1
+	public String solution1(String number, int k) {
+		String answer = "";
+
+		return answer;
+	}
+}
