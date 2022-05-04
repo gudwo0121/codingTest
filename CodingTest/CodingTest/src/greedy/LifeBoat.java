6110aead627dc1ad9f6fc72f782bb447a8e3c2c5
BRO <91590390+gudwo0121@users.noreply.github.com>
2022-05-04 17:54:50 +0900

    22.05.04.LifeBoat.Linked

---
+++ CodingTest/CodingTest/src/greedy/LifeBoat.java
@@ -0,0 +1,27 @@
+package greedy;
+
+public class LifeBoat {
+
+	// case 1
+	static int[] people = { 70, 50, 80, 50 };
+	static int limit = 100;
+
+	// case 2
+//	static int[] people = { 70, 80, 50 };
+//	static int limit = 100;
+
+	public static void main(String[] args) {
+		LifeBoat lifeBoat = new LifeBoat();
+		int answer = lifeBoat.solution1(people, limit);
+		System.out.println(answer);
+	}
+
+	// solution 1
+	public int solution1(int[] people, int limit) {
+		int answer = 0;
+		
+		
+
+		return answer;
+	}
+}
