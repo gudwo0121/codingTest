47b6669cddb12f19ec0a9bcd63b5a4588da01c02
BRO <91590390+gudwo0121@users.noreply.github.com>
2022-06-02 17:25:03 +0900

    22.06.02.IntTriangle.Linked

---
+++ CodingTest/CodingTest/src/dynamic/IntTriangle.java
@@ -0,0 +1,20 @@
+package dynamic;
+
+public class IntTriangle {
+
+	// case 1
+	static int[][] triangle = { { 7 }, { 3, 8 }, { 8, 1, 0 }, { 2, 7, 4, 4 }, { 4, 5, 2, 6, 5 } };
+
+	public static void main(String[] args) {
+		IntTriangle intTriangle = new IntTriangle();
+		int answer = intTriangle.solution1(triangle);
+		System.out.println(answer);
+	}
+
+	// solution 1
+	public int solution1(int[][] triangle) {
+		int answer = 0;
+
+		return answer;
+	}
+}
