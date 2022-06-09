4b3bfd6dc01572c4ff26a6e397f01fff2163bba6
BRO <91590390+gudwo0121@users.noreply.github.com>
2022-06-09 18:15:36 +0900

    22.06.09.IntTriangle.Solved

--- CodingTest/CodingTest/src/dynamic/IntTriangle.java
+++ CodingTest/CodingTest/src/dynamic/IntTriangle.java
@@ -12,9 +12,23 @@ public class IntTriangle {
 	}
 
 	// solution 1
+	// 삼각형 맨 아래 층부터 역으로 올라오며 Max값을 구하는 방식
+	// Bottom-up Dynamic Programming
 	public int solution1(int[][] triangle) {
 		int answer = 0;
 
+		// 삼각형의 계층 (감소 = 아래서 위로)
+		for (int i = triangle.length - 1; i >= 1; i--) {
+			// 계층 내 더해질 수 있는 수
+			for (int j = 1; j < i + 1; j++) {
+				// 그 중에 더 큰 수를 상위 계층에 덮어씌우기
+				int a = triangle[i - 1][j - 1] + triangle[i][j - 1];
+				int b = triangle[i - 1][j - 1] + triangle[i][j];
+				triangle[i - 1][j - 1] = Math.max(a, b);
+				// 최종적으로 더해진 값이 최대 값
+				answer = Math.max(a, b);
+			}
+		}
 		return answer;
 	}
 }
