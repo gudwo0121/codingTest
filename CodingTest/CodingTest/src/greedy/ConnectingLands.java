3aed4d9e38ced1d512c9743d97140c7ad220ed87
BRO <91590390+gudwo0121@users.noreply.github.com>
2022-06-14 21:24:16 +0900

    22.06.14.ConnectingLands.Linked

---
+++ CodingTest/CodingTest/src/greedy/ConnectingLands.java
@@ -0,0 +1,23 @@
+package greedy;
+
+public class ConnectingLands {
+
+	// case 1
+	static int n = 4;
+	static int[][] costs = { { 0, 1, 1 }, { 0, 2, 2 }, { 1, 2, 5 }, { 1, 3, 1 }, { 2, 3, 8 } };
+
+	public static void main(String[] args) {
+		ConnectingLands connectingLands = new ConnectingLands();
+		int answer = connectingLands.solution1(n, costs);
+		System.out.println(answer);
+	}
+
+	// solution 1
+	public int solution1(int n, int[][] costs) {
+		int answer = 0;
+		
+		
+		
+		return answer;
+	}
+}
